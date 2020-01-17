2ce386563a28d40774882ff16a56d3e5422e5dd8
Nab <nab@codetrainer.fr>
2020-01-17 23:21:12 +0100

    Add project files.

---
+++ Disposable/LogFileWriter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disposable
+{
+    public class LogFileWriter : IDisposable
+    {
+        protected bool isDisposed = false;
+
+        ~LogFileWriter()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool isDisposing)
+        {
+            if (!isDisposed)
+            {
+                if (isDisposing)
+                {
+                    // Nettoyage ressources managées;
+                }
+
+                // Nettoyage ressources non managées;
+
+                isDisposed = true;
+
+                //base.Dispose(isDisposing); //uniquement si l'objet hérite d'un autre objet disposable
+            }
+        }
+    }
+
+}
---
+++ Disposable/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disposable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogFileWriter log = new LogFileWriter();
+
+            //-----1ere solution-----
+            //LogFileWriter log = new LogFileWriter();
+
+            //try
+            //{
+            //    //utiliser l'objet log
+            //}
+            //finally
+            //{
+            //    //liberation des ressources managées
+            //    //et non managées
+            //    log.Dispose();
+            //}
+
+            //-----2eme solution-----
+            //using (LogFileWriter log = new LogFileWriter())
+            //{
+            //    //utiliser l'objet log
+
+            //}//appel implicite à Dispose()
+
+            Console.ReadLine();
+        }
+    }
+
+
+}
